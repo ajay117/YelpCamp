2ddea9eedc5b4ea41fd7a3539bcd634e03c60e74
Ajay KC <meajay64@gmail.com>
2022-03-20 22:17:08 +0900

    Handled more errors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const ExpressError = require("./utils/ExpressError");
 // const { nextTick } = require("process");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -32,6 +33,8 @@ app.get("/", (req, res) => {
 app.get(
   "/campgrounds",
   catchAsync(async (req, res) => {
+    if (!req.body.campground)
+      throw new ExpressError("Invalid Campground Data", 404);
     const campgrounds = await Campground.find();
     res.render("campgrounds/index", { campgrounds });
   })
@@ -44,13 +47,9 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
-    try {
-      const campground = new Campground(req.body.campground);
-      await campground.save();
-      res.redirect(`/campgrounds/${campground._id}`);
-    } catch (e) {
-      next(e);
-    }
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
@@ -89,8 +88,13 @@ app.delete(
   })
 );
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not found", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("Oh boy, something went wrong");
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
