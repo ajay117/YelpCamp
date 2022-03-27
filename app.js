0f5003fc468d9dcbeb5562433d6617f414e08d9e
Ajay KC <meajay64@gmail.com>
2022-03-27 16:39:22 +0900

    Added new route so that review by user can be added to databases

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const catchAsync = require("./utils/catchAsync");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const ExpressError = require("./utils/ExpressError");
-// const { nextTick } = require("process");
+const Review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -96,6 +96,18 @@ app.delete(
     const id = req.params.id;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+app.post(
+  "/campgrounds/:id/reviews",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
