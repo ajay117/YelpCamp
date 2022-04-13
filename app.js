958583e6df9d39f9bd4f0ba1cca33cd628635cbe
Ajay KC <meajay64@gmail.com>
2022-04-13 20:49:35 +0900

    Setting flash

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require("./utils/catchAsync");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const session = require("express-session");
+const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const Review = require("./models/review");
 
@@ -43,6 +44,13 @@ const sessionConfig = {
   },
 };
 app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
