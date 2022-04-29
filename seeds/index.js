eb787672130300dbb40a959bb22b8e42511bd82f
Ajay KC <meajay64@gmail.com>
2022-04-30 08:30:23 +0900

    Fixing our seeds

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,16 @@ const seedDB = async () => {
       author: "625ba6d17e6b079e06724dbf",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: `${randomImage}`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/yelpcamp619/image/upload/v1651271207/YelpCamp/nbnz9chjb53uxspktg5a.jpg",
+          filename: "YelpCamp/nbnz9chjb53uxspktg5a",
+        },
+        {
+          url: "https://res.cloudinary.com/yelpcamp619/image/upload/v1651271207/YelpCamp/xjyvv2mjrgceozjb7mxh.jpg",
+          filename: "YelpCamp/xjyvv2mjrgceozjb7mxh",
+        },
+      ],
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
     });
