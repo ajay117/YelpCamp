00556c3a5277e61e2941b166683d4e25d611cdfe
Ajay KC <meajay64@gmail.com>
2022-04-30 14:48:33 +0900

    Customizing file input

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,3 +1,4 @@
+const campground = require("../models/campground");
 const Campground = require("../models/campground");
 
 module.exports.index = async (req, res) => {
@@ -50,11 +51,17 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const id = req.params.id;
-  const camp = await Campground.findByIdAndUpdate(id, {
+  const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+  const imgs = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.images.push(...imgs);
+  await campground.save();
   req.flash("success", "Successfully updated campground");
-  res.redirect(`/campgrounds/${camp._id}`);
+  res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.deleteCampground = async (req, res) => {
